bf9a15d25f6625383355c1b04c46a0b14a952b08
Mitko <mitkomitrov09@gmail.com>
2026-08-02 21:30:01 +0200

    feat: register PostgreSQL database context

--- src/Commerce.Api/Program.cs
+++ src/Commerce.Api/Program.cs
@@ -1,8 +1,19 @@
+using Commerce.Api.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("CommerceDatabase");
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'CommerceDatabase' is required.");
+}
+
+builder.Services.AddDbContext<CommerceDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
